8854394f58c744a48c5d73a68a92c8024c522661
Brandy Carney <brandy@ionic.io>
2023-05-15 17:19:06 -0400

    test(segment): remove short text tests

--- core/src/components/segment/test/wrap/segment.e2e.ts
+++ core/src/components/segment/test/wrap/segment.e2e.ts
@@ -3,28 +3,6 @@ import { configs, test } from '@utils/test/playwright';
 
 configs().forEach(({ title, screenshot, config }) => {
   test.describe(title('segment: wrap'), () => {
-    test('should not wrap short text', async ({ page }) => {
-      await page.setContent(
-        `
-        <ion-segment>
-          <ion-segment-button value="bookmarks">
-            <ion-label>Bookmarks</ion-label>
-          </ion-segment-button>
-          <ion-segment-button value="favorites">
-            <ion-label>Favorites</ion-label>
-          </ion-segment-button>
-          <ion-segment-button value="shared">
-            <ion-label>Shared</ion-label>
-          </ion-segment-button>
-        </ion-segment>
-      `,
-        config
-      );
-
-      const segment = page.locator('ion-segment');
-
-      await expect(segment).toHaveScreenshot(screenshot(`segment-no-wrap`));
-    });
     test('should wrap long text', async ({ page }) => {
       await page.setContent(
         `
@@ -96,30 +74,6 @@ configs().forEach(({ title, screenshot, config }) => {
       const segment = page.locator('ion-segment');
 
       await expect(segment).toHaveScreenshot(screenshot(`segment-wrap-icon-start`));
-    });
-    test('should not wrap short text in a toolbar', async ({ page }) => {
-      await page.setContent(
-        `
-        <ion-toolbar>
-          <ion-segment>
-            <ion-segment-button value="bookmarks">
-              <ion-label>Bookmarks</ion-label>
-            </ion-segment-button>
-            <ion-segment-button value="favorites">
-              <ion-label>Favorites</ion-label>
-            </ion-segment-button>
-            <ion-segment-button value="shared">
-              <ion-label>Shared</ion-label>
-            </ion-segment-button>
-          </ion-segment>
-        </ion-toolbar>
-      `,
-        config
-      );
-
-      const toolbar = page.locator('ion-toolbar');
-
-      await expect(toolbar).toHaveScreenshot(screenshot(`segment-no-wrap-toolbar`));
     });
     test('should wrap long text in a toolbar', async ({ page }) => {
       await page.setContent(
